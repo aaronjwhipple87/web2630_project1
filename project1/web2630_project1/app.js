01ef756ab7a11d3ec85402bb0945b0db0c539b33
ohhellwhipple <39172970+ohhellwhipple@users.noreply.github.com>
2018-09-20 11:16:13 -0600

    Add files via upload

--- project1/web2630_project1/app.js
+++ project1/web2630_project1/app.js
@@ -6,7 +6,6 @@ new Vue({
       highScore: 10,
       showGame: true,
       resultLog: [],
-      highScoreReached: false,
 
   },
   methods: {
@@ -22,124 +21,117 @@ new Vue({
           this.userWins = 0;
           this.compWins = 0;
           this.showGame = !this.showGame;
-          this.highScore  = 0;
           this.resultLog = [];
-          this.highScoreReached = !this.highScoreReached;
       },
 
       rockChoice: function(){
           let computerChoice = this.compChoice();
-
+          if (computerChoice === "paper") {
+              this.resultLog.unshift({
+                  compWin: true,
+                  tie: false,
+                  text: "COMPUTER CHOSE PAPER | PAPER BEATS ROCK | COMPUTER WINS!"
+              });
+              this.compWins++;
+          } else if (computerChoice === "scissors") {
+              this.resultLog.unshift({
+                  compWin: false,
+                  tie: false,
+                  text: "COMPUTER CHOSE SCISSORS | ROCK BEATS SCISSORS | YOU WIN!"
+              });
+              this.userWins++;
+          } else {
+              this.resultLog.unshift({
+                  compWin: false,
+                  tie: true,
+                  text: "COMPUTER CHOSE ROCK | YOU'VE TIED!"
+              });
+          }
+          this.gamesWinner();
       },
 
       paperChoice: function(){
           let computerChoice = this.compChoice();
-
+          if (computerChoice === "scissors") {
+              this.resultLog.unshift({
+                  compWin: true,
+                  tie: false,
+                  text: "COMPUTER CHOSE SCISSORS | SCISSORS BEATS PAPER | COMPUTER WINS!"
+              });
+              this.compWins++;
+          } else if (computerChoice === "rock") {
+              this.resultLog.unshift({
+                  compWin: false,
+                  tie: false,
+                  text: "COMPUTER CHOSE ROCK | PAPER BEATS ROCK | YOU WIN!"
+              });
+              this.userWins++;
+          } else {
+              this.resultLog.unshift({
+                  compWin: false,
+                  tie: true,
+                  text: "COMPUTER CHOSE PAPER | YOU'VE TIED!"
+              });
+          }
+          this.gamesWinner();
       },
 
       scissorChoice: function(){
           let computerChoice = this.compChoice();
-
+          if (computerChoice === "rock") {
+              this.resultLog.unshift({
+                  compWin: true,
+                  tie: false,
+                  text: "COMPUTER CHOSE ROCK | ROCK BEATS SCISSORS | COMPUTER WINS!"
+              });
+              this.compWins++;
+          } else if (computerChoice === "paper") {
+              this.resultLog.unshift({
+                  compWin: false,
+                  tie: false,
+                  text: "COMPUTER CHOSE PAPER | SCISSORS BEATS PAPER | YOU WIN!"
+              });
+              this.userWins++;
+          } else {
+              this.resultLog.unshift({
+                  compWin: false,
+                  tie: true,
+                  text: "COMPUTER CHOSE SCISSORS | YOU'VE TIED!"
+              });
+          }
+          this.gamesWinner();
       },
 
+      //generate computer choice
       compChoice: function(){
           let computerChoice = Math.random();
 
-
           if(computerChoice < 0.34){
-              computerChoice = "rock";
+               return "rock";
           }else if(computerChoice > 0.34 && computerChoice < 0.67){
-              computerChoice = "paper"
+              return "paper";
           }else{
-              computerChoice = "scissors"
-          }
-      },
-
-
-      //compare individual games
-      compare: function(userChoice,computerChoice){
-          if (userChoice === "rock") {
-              if (computerChoice === "paper") {
-                  this.resultLog.unshift({
-                      compWin: true,
-                      tie: false,
-                      text: "COMPUTER CHOSE PAPER | PAPER BEATS ROCK | COMPUTER WINS!"
-                  });
-                  this.compWins++;
-              } else if (computerChoice === "scissors") {
-                  this.resultLog.unshift({
-                      compWin: false,
-                      tie: false,
-                      text: "COMPUTER CHOSE SCISSORS | ROCK BEATS SCISSORS | YOU WIN!"
-                  });
-                  this.userWins++;
-              } else {
-                  this.resultLog.unshift({
-                      compWin: false,
-                      tie: true,
-                      text: "COMPUTER CHOSE ROCK | YOU'VE TIED!"
-                  });
-              }
-          } else if (userChoice === "paper") {
-              if (computerChoice === "scissors") {
-                  this.resultLog.unshift({
-                      compWin: true,
-                      tie: false,
-                      text: "COMPUTER CHOSE SCISSORS | SCISSORS BEATS PAPER | COMPUTER WINS!"
-                  });
-                  this.compWins++;
-              } else if (computerChoice === "rock") {
-                  this.resultLog.unshift({
-                      compWin: false,
-                      tie: false,
-                      text: "COMPUTER CHOSE ROCK | PAPER BEATS ROCK | YOU WIN!"
-                  });
-                  this.userWins++;
-              } else {
-                  this.resultLog.unshift({
-                      compWin: false,
-                      tie: true,
-                      text: "COMPUTER CHOSE PAPER | YOU'VE TIED!"
-                  });
-              }
-          } else {
-              if (computerChoice === "rock") {
-                  this.resultLog.unshift({
-                      compWin: true,
-                      tie: false,
-                      text: "COMPUTER CHOSE ROCK | ROCK BEATS SCISSORS | COMPUTER WINS!"
-                  });
-                  this.compWins++;
-              } else if (computerChoice === "paper") {
-                  this.resultLog.unshift({
-                      compWin: false,
-                      tie: false,
-                      text: "COMPUTER CHOSE PAPER | SCISSORS BEATS PAPER | YOU WIN!"
-                  });
-                  this.userWins++;
-              } else {
-                  this.resultLog.unshift({
-                      compWin: false,
-                      tie: true,
-                      text: "COMPUTER CHOSE SCISSORS | YOU'VE TIED!"
-                  });
-              }
+              return "scissors";
           }
       },
 
       //compare if comp won or user
-      gamesResults: function(highScore){
-          if(this.highScore == this.compWins || this.highScore == this.userWins){
-              if(this.compWins > this.userWins){
-
-              }else {
-
-              }
-
-              this.highScore = 0;
-              this.compWins = 0;
+      gamesWinner: function(){
+          if(this.userWins === 10) {
+              alert("You Won!");
               this.userWins = 0;
+              this.compWins = 0;
+              this.showGame = !this.showGame;
+              this.resultLog = [];
           }
+          else if(this.compWins === 10) {
+              alert("Computer Wins! Sorry you Lose!");
+              this.userWins = 0;
+              this.compWins = 0;
+              this.showGame = !this.showGame;
+              this.resultLog = [];
+          }
+
       }
 
 
